5410b671eaecf865c2b2a6d752e17d77cf99ee92
Eric Yan <yaneric.4d@gmail.com>
2023-04-09 12:55:41 -0700

    add login feature for frontend, updatelogin forms

--- client/src/routes/Login.tsx
+++ client/src/routes/Login.tsx
@@ -11,7 +11,12 @@ const Login = () => {
         gender: ''
     });
 
-    const handleOnChange = (e: any) => {
+    const [loginData, setLoginData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const handleOnChangeRegister = (e: any) => {
         e.preventDefault();
         const newFormData = registerData;
         const { value, name } = e.target;
@@ -41,6 +46,21 @@ const Login = () => {
         setRegisterData(newFormData);
     }
 
+    const handleOnChangeLogin = (e: any) => {
+        e.preventDefault();
+        const newFormData = loginData;
+        const { value, name } = e.target;
+        switch (name) {
+            case 'email':
+                newFormData.email = value;
+                break;
+            case 'password':
+                newFormData.password = value;
+                break;
+        }
+        setLoginData(newFormData);
+    }
+
     // TODO: Add Submit function to submit to backend for register user
     const registerSubmit = async (e: any) => {
         e.preventDefault();
@@ -54,15 +74,42 @@ const Login = () => {
         })
         .then((response => {
             if(!response.ok) {
-                throw new Error(`HTTP error, status code: ${response.status}`);
+                throw new Error(`status code: ${response.status} - Email may already exist`);
             }
             return response;
         }))
         .then((response) => response.json())
-        .catch((error) => console.log(error));
+        .then(() => alert('Registered'))
+        .catch((error) => alert(error));
     }
 
     // TODO: Add Submit function to submit to backend for login user (JWT)
+    const loginSubmit = async (e: any) => {
+        e.preventDefault();
+        const response = await fetch(`http://localhost:5000/api/users/login`, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            method: 'POST',
+            body: JSON.stringify(loginData),
+        })
+        .then(response => {
+            if(!response.ok) {
+                alert('Invalid login')
+                return;
+            }
+            return response.json();
+        })
+        .then((data) => {
+            if(data) {
+                localStorage.setItem('token', data.token);
+                alert('successful login');
+            }
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
 
     return (
         <div className='container my-12 mx-auto px-4 md:px-12 overflow-auto'>
@@ -70,36 +117,36 @@ const Login = () => {
             <h1 className='text-3xl mt-4 mb-4'>Register</h1>
             <form className='flex flex-col' onSubmit={registerSubmit}>
                 <label className=''>Email:
-                    <input type='email' name='email' onChange={handleOnChange} className='mx-4 my-2 border border-black'></input>
+                    <input type='email' name='email' onChange={handleOnChangeRegister} className='mx-4 my-2 border border-black'></input>
                 </label>
                 <label>Password:
-                    <input type='password' name='password' onChange={handleOnChange} className='mx-4 my-2 border border-black'></input>
+                    <input type='password' name='password' onChange={handleOnChangeRegister} className='mx-4 my-2 border border-black'></input>
                 </label>
                 <label>First Name:
-                    <input type='text' name='first_name' onChange={handleOnChange} className='mx-4 my-2 border border-black'></input>
+                    <input type='text' name='first_name' onChange={handleOnChangeRegister} className='mx-4 my-2 border border-black'></input>
                 </label>
                 <label>Last Name:
-                    <input type='text' name='last_name' onChange={handleOnChange} className='mx-4 my-2 border border-black'></input>
+                    <input type='text' name='last_name' onChange={handleOnChangeRegister} className='mx-4 my-2 border border-black'></input>
                 </label>
                 <label>Date of Birth:
-                    <input type='date' name='date_of_birth' onChange={handleOnChange} className='mx-4 my-2 border border-black'></input>
+                    <input type='date' name='date_of_birth' onChange={handleOnChangeRegister} className='mx-4 my-2 border border-black'></input>
                 </label>
                 <label>Home Town:
-                    <input type='text' name='hometown' onChange={handleOnChange} className='mx-4 my-2 border border-black'></input>
+                    <input type='text' name='hometown' onChange={handleOnChangeRegister} className='mx-4 my-2 border border-black'></input>
                 </label>
                 <label>Gender:
-                    <input type='text' name='gender' onChange={handleOnChange} className='mx-4 my-2 border border-black'></input>
+                    <input type='text' name='gender' onChange={handleOnChangeRegister} className='mx-4 my-2 border border-black'></input>
                 </label>
                 <input type='submit' value='Register' className='bg-gray-900 text-gray-300 p-2 rounded w-28 hover:bg-gray-700'></input>
             </form>
 
             <h1 className='text-3xl mt-4 mb-4'>Login</h1>
-            <form className='flex flex-col'>
+            <form className='flex flex-col' onSubmit={loginSubmit}>
                 <label>Email:
-                    <input type='email' className='mx-4 my-2 border border-black'></input>
+                    <input type='email' name='email' onChange={handleOnChangeLogin} className='mx-4 my-2 border border-black'></input>
                 </label>
                 <label>Password
-                    <input type='password' className='mx-4 my-2 border border-black'></input>
+                    <input type='password' name='password' onChange={handleOnChangeLogin} className='mx-4 my-2 border border-black'></input>
                 </label>
                 <input type='submit' value='Login' className='bg-gray-900 text-gray-300 p-2 rounded w-28 hover:bg-gray-700'></input>
             </form>
